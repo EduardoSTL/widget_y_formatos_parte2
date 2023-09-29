11b1e30aba4cdbb973f576e9cba6c28cf3f727eb
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-29 11:30:32 -0600

    moved cards screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:widget_y_formatos/presentation/cards/cards_screen.dart';
+import 'package:widget_y_formatos/presentation/screens/cards/cards_screen.dart';
 import 'package:widget_y_formatos/presentation/screens/buttons/buttons_screen.dart';
 import 'package:widget_y_formatos/presentation/screens/home/home_screens.dart';
 
