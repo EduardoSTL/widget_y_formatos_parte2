88f88f460b4e91d82a163e7d70d60ca1f61b3b3f
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-29 11:25:52 -0600

    added: router dependency

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,29 @@
+import 'package:go_router/go_router.dart';
+import 'package:widget_y_formatos/presentation/cards/cards_screen.dart';
+import 'package:widget_y_formatos/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widget_y_formatos/presentation/screens/home/home_screens.dart';
+
+//* Go router config
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(), 
+    ),
+
+    GoRoute(
+      path: '/buttons',
+      name: HomeScreen.name,
+      builder: (context, state) => const ButtonsScreen(), 
+    ),
+
+    GoRoute(
+      path: '/cards',
+      name: HomeScreen.name,
+      builder: (context, state) => const CardsScreen(), 
+    ),
+  ],
+);
