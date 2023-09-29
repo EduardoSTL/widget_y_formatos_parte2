dba464d8887e3939ae4429a8eed96a33351f7f21
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-29 12:21:02 -0600

    exports

---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,9 @@
+export 'package:widget_y_formatos/presentation/screens/animated/animated_screen.dart';
+export 'package:widget_y_formatos/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widget_y_formatos/presentation/screens/buttons/buttons_screen.dart';
+export 'package:widget_y_formatos/presentation/screens/cards/cards_screen.dart';
+export 'package:widget_y_formatos/presentation/screens/home/home_screen.dart';
+export 'package:widget_y_formatos/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widget_y_formatos/presentation/screens/progress/progress_screen.dart';
+export 'package:widget_y_formatos/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widget_y_formatos/presentation/screens/ui_controls/ui_controls_screen.dart';
