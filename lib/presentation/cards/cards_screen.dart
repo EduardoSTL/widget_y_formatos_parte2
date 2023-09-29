f67fedd483b938f48d978ef0cd4428d66e35a587
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-29 10:53:07 -0600

    added: cardtype 1 and 2

--- lib/presentation/cards/cards_screen.dart
+++ lib/presentation/cards/cards_screen.dart
@@ -76,6 +76,97 @@ class _CardType1 extends StatelessWidget {
                 icon: const Icon(Icons.more_vert_outlined),
                 onPressed: (){},
               ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(label),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+
+  final String label;
+  final double elevation;
+
+  const _CardType2({
+    required this.label,
+    required this.elevation
+    });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        side: BorderSide(
+          color: colors.outline
+        )
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: (){},
+              ),
+            ),
+
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+
+  final String label;
+  final double elevation;
+
+  const _CardType3({
+    required this.label,
+    required this.elevation
+    });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: (){},
+              ),
+            ),
+
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled'),
             )
           ],
         ),
