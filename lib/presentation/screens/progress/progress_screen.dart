873bce938d9d1412b5498136618c96e8264b442f
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-02 12:01:46 -0600

    updt: progress screen

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -13,18 +13,61 @@ class ProgressScreen extends StatelessWidget {
         title: const Text('Progress Indicator'),
 
       ),
-      body: _ProgressView(),
+      body: const _ProgressView(),
     );
   }
 }
 
 class _ProgressView extends StatelessWidget {
 
-
-  const _ProgressView({super.key});
+  const _ProgressView();
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Center(
+      child: Column(
+        children: const [
+        SizedBox(height: 30),
+        Text('Circula progress indicator'),
+        SizedBox(height: 10),
+        CircularProgressIndicator(strokeWidth: 2, backgroundColor: Colors.black45),
+        SizedBox(height: 20),
+        Text('Circular y Linear controlado'),
+        SizedBox(height: 10),
+        _ControlledProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value){
+        return (value * 2) / 100; // 0.0, 1.0
+        }).takeWhile((value) => value < 100),
+        builder: (context, snapshot){
+          final progressValue = snapshot.data ?? 0;
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(value: progressValue, strokeWidth: 2, backgroundColor: Colors.black12),
+                const SizedBox(
+                  width: 20,),
+                Expanded(
+                  child: LinearProgressIndicator(value: progressValue)
+                ),
+              ],
+            ),  
+          );
+        },
+    );
   }
 }
