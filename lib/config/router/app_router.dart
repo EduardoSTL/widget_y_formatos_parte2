ad94eff1cf8643deed86ca262397888af67b23d2
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-02 11:26:39 -0600

    added progress to app router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:widget_y_formatos/presentation/screens/cards/cards_screen.dart';
 import 'package:widget_y_formatos/presentation/screens/buttons/buttons_screen.dart';
 import 'package:widget_y_formatos/presentation/screens/home/home_screen.dart';
+import 'package:widget_y_formatos/presentation/screens/screens.dart';
 
 //* GoRouter config
 final appRouter = GoRouter(
@@ -25,5 +26,23 @@ final appRouter = GoRouter(
       name: CardsScreen.name,
       builder: (context, state) => const CardsScreen(), 
     ),
+
+    GoRoute(
+      path: '/progress',
+      name: ProgressScreen.name,
+      builder: (context, state) => const ProgressScreen(), 
+    ),
+
+    /* GoRoute(
+      path: '/snackbars',
+      name: SnackbarScreen.name,
+      builder: (context, state) => const SnackbarScreen(), 
+    ),
+
+    GoRoute(
+      path: '/infinitescroll',
+      name: InfiniteScrollScreen.name,
+      builder: (context, state) => const InfiniteScrollScreen(), 
+    ), */
   ],
 );
