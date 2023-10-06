e2f4a6e787093aabe2a22e7d145419fa0acf7436
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-06 08:21:09 -0600

    added: provider package

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -16,17 +16,27 @@ const colorList = <Color>[
 
 class AppTheme{
   final int selectedColor;
-  
-  AppTheme({
-    this.selectedColor = 0
-  }) : assert(selectedColor >= 0, 'Selected color must be greater than 0'),
-      assert(selectedColor < colorList.length, 'Selected color must be less or equal than ${colorList.length -1}');
+  final bool isDarkmode;
 
+  AppTheme({
+    this.selectedColor=0,
+    this.isDarkmode = false,
+  }): assert(selectedColor >=0, 'Selected color must be greater then 0'),
+  assert(selectedColor <= colorList.length, 'Selected color must be less or equal tha ${colorList.length-1}') ;
+  
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
     colorSchemeSeed:  colorList[selectedColor],
     appBarTheme: const AppBarTheme(
       centerTitle: false
     )
+  );
+
+  AppTheme copyWith({
+    int? selectedColor,
+    bool? isDarkmode
+  }) => AppTheme(
+    selectedColor: selectedColor ?? this.selectedColor,
+    isDarkmode: isDarkmode ?? this.isDarkmode,
   );
 }
---
+++ lib/presentation/provider/counter_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod/riverpod.dart';
+
+final counterProvider = StateProvider<int>((ref) => 5);
+
